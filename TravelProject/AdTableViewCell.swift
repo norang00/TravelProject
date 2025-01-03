625c607f041704164e5ce323ed83c1ddbe3fe1dd
norang <norang1221@gmail.com>
2025-01-03 23:50:55 +0900

    - Travel 탭 레이아웃 구성

---
+++ TravelProject/AdTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AdTableViewCell.swift
+//  TravelProject
+//
+//  Created by Kyuhee hong on 1/3/25.
+//
+
+import UIKit
+
+class AdTableViewCell: UITableViewCell {
+
+    @IBOutlet var adContentView: UIView!
+    @IBOutlet var adContentLabel: UILabel!
+    @IBOutlet var adBadgeLabel: UILabel!
+}
---
+++ TravelProject/TravelTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TravelTableViewCell.swift
+//  TravelProject
+//
+//  Created by Kyuhee hong on 1/3/25.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var detailInfoLabel: UILabel!
+    @IBOutlet var cityImageView: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    
+}
+
+
+//Travel(title: "도쿄 디즈니랜드",
+//       description: "월트 디즈니가 창조한 동화 속 세상",
+//       travel_image: "https://images.unsplash.com/photo-1545580492-8859ba8323f0?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//       grade: 4.7,
+//       save: 26076,
+//       like: true,
+//       ad: false),
