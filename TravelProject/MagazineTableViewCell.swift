43563ac311f0aab8c77410ea1d43ea09ef0d2307
norang <norang1221@gmail.com>
2025-01-03 19:24:59 +0900

    - MagazineTable 탭 생성, TableViewController, TableViewCell 파일 추가

---
+++ TravelProject/MagazineTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MagazineTableViewCell.swift
+//  TravelProject
+//
+//  Created by Kyuhee hong on 1/3/25.
+//
+
+import UIKit
+
+class MagazineTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
