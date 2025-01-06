fcc606cd6ed84a1a312c274f6e9507315a746bed
norang <norang1221@gmail.com>
2025-01-06 21:14:40 +0900

    -  City 탭 관련 파일 추가, 모델 생성, 더미 데이터 추가

---
+++ TravelProject/CityViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CityViewCell.swift
+//  TravelProject
+//
+//  Created by Kyuhee hong on 1/6/25.
+//
+
+import UIKit
+
+class CityViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TravelProject/Model/City.swift
@@ -0,0 +1,35 @@
+//
+//  City.swift
+//  TravelProject
+//
+//  Created by Kyuhee hong on 1/6/25.
+//
+import UIKit
+
+struct City {
+    static let defaultImage: UIImage = UIImage(named:"defaultImage")!
+    
+    let city_name: String
+    let city_english_name: String
+    let city_explain: String
+    let city_image: String
+    let domestic_travel: Bool
+    
+    init(city_name: String, city_english_name: String, city_explain: String, city_image: String, domestic_travel: Bool) {
+        self.city_name = city_name
+        self.city_english_name = city_english_name
+        self.city_explain = city_explain
+        self.city_image = city_image
+        self.domestic_travel = domestic_travel
+    }
+    
+    var imageURL: URL? {
+        return URL(string: city_image ?? "")
+    }
+    
+    var detailInfo: String {
+        return ""
+    }
+}
+
+
